ed2a44e3c4bcadcede3b096acae590f76fe30f83
ASUS <ASUS@HOME-PC>
2023-01-24 14:32:34 +0300

    Add project files.

---
+++ MyProj/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region 2 - MISOL 
+int k = int.Parse(Console.ReadLine());
+switch (k)
+{
+    case 1:
+        Console.WriteLine("Yomon bola"); break;
+
+
+    case 2:
+        Console.WriteLine(" Qoniqarsiz bola"); break;
+
+
+    case 3:
+        Console.WriteLine("Qoniqarli o'quvchi"); break;
+
+    case 4:
+        Console.WriteLine(" Yaxshi o'quvchi "); break;
+
+    case 5:
+        Console.WriteLine(" a'lo o'qvchi"); break;
+
+    default:
+        Console.WriteLine(" bunaqa amal yuq"); break;
+}
+#endregion
